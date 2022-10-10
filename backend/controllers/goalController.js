7d92c52d9f33fc5091d7ed1f598f9177b3e2e8e1
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-10-10 12:44:01 +0900

    goals/without_user

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -90,9 +90,87 @@ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ resData })
 })
 
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
+const getGoalsWithoutUser = asyncHandler(async (req, res) => {
+    const resData = await Goal.findAll();
+    console.log({ resData })
+
+    res.status(200).json(resData)
+})
+
+// @desc    Set goal
+// @route   POST /api/goals
+// @access  Private
+const setGoalWithoutUser = asyncHandler(async (req, res) => {
+    const inputData = {
+        text: req.body.text
+    }
+
+    if (!inputData.text) {
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
+    const resData = await Goal.create(inputData)
+
+    // console.log({ resData })
+
+    res.status(200).json(resData)
+})
+
+// @desc    Update goal
+// @route   PUT /api/goals/:id
+// @access  Private
+const updateGoalWithoutUser = asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const inputData = {
+        text: req.body.text
+    }
+
+    const goal = await Goal.findByPk(id);
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const resData = await Goal.update(inputData, {
+        where: { id }
+    });
+
+    // console.log({ resData })
+
+    res.status(200).json(resData)
+})
+
+// @desc    Delete goal
+// @route   DELETE /api/goals/:id
+// @access  Private
+const deleteGoalWithoutUser = asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const goal = await Goal.findByPk(id);
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const resData = await Goal.destroy({
+        where: { id }
+    });
+
+    res.status(200).json({ resData })
+})
+
 module.exports = {
     getGoals,
     setGoal,
     updateGoal,
     deleteGoal,
+    getGoalsWithoutUser,
+    setGoalWithoutUser,
+    updateGoalWithoutUser,
+    deleteGoalWithoutUser,
 }
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,6 +5,10 @@ const {
     setGoal,
     updateGoal,
     deleteGoal,
+    getGoalsWithoutUser,
+    setGoalWithoutUser,
+    updateGoalWithoutUser,
+    deleteGoalWithoutUser,
 } = require('../controllers/goalController')
 
 const { protect } = require('../middleware/authMiddleware')
@@ -12,7 +16,7 @@ const { protect } = require('../middleware/authMiddleware')
 router.route('/').get(protect, getGoals).post(protect, setGoal)
 router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
-// router.route('/').get(getGoals).post(setGoal)
-// router.route('/:id').delete(deleteGoal).put(updateGoal)
+router.route('/without_user').get(getGoalsWithoutUser).post(setGoalWithoutUser)
+router.route('/without_user/:id').delete(deleteGoalWithoutUser).put(updateGoalWithoutUser)
 
 module.exports = router
